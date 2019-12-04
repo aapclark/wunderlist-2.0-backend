0ad9ffd73d2e2d76ce797f14984fdaf3680c50cd
Aaron Clark <aaronpclark92@gmail.com>
2019-12-03 18:14:21 -0600

    project scaffolding, dependencies and dev-dependencies added, configured knexfile

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: `./data/migrations`
+    },
+    seeds: {
+      directory: `./data/seeds`
+    },
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run(`PRAGMA foreign_keys = ON`, done)
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: `./data/test/test.db3`,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: `./data/migrations`
+    },
+    seeds: {
+      directory: `./data/seeds`
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+
+    migrations: {
+      directory: `./data/migrations`,
+    },
+    seeds: {
+      directory: `./data/migrations`,
+    },
+  },
+
+};
